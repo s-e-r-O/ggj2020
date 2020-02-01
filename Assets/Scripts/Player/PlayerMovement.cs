b47e29a03bc648e1fd74f62183b2ed5da4092d07
Sergio Oporto <ser.op.te@gmail.com>
2020-02-01 00:15:50 -0400

    Add basic ball movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Collision coll;
+
+    [Header("Stats")]
+
+    public float speed = 30f;
+    public float jumpForce = 30f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collision>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 dir = new Vector2(x, y);
+        Walk(dir);
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (coll.onGround)
+            {
+                Jump(Vector2.up);
+            }
+        }
+    }
+
+    private void Walk(Vector2 dir)
+    {
+        rb.velocity = (new Vector2(dir.x * speed, rb.velocity.y));
+    }
+
+    private void Jump(Vector2 dir)
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.velocity += dir * jumpForce;
+    }
+}
