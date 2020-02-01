6476404e88b0736c7dcdcc1cd57151f9c9a9e74c
Kevin Aguilar <kevin.16.mn@gmail.com>
2020-02-01 13:02:00 -0400

    Add score manager and canvas

---
+++ Assets/Scripts/Player/Collector.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Collector : MonoBehaviour
+{
+    private int items = 0;
+    public bool player;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Items"))
+        {
+            items++;
+            if (player)
+            {
+                ScoreManager.instance.ChangeScorePlayer1(items);
+            }
+            else
+            {
+                ScoreManager.instance.ChangeScorePlayer2(items);
+            }
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Score/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+    public TextMeshProUGUI ScorePlayer1;
+    public TextMeshProUGUI ScorePlayer2;
+
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void ChangeScorePlayer1(int value)
+    {
+        ScorePlayer1.text = "x" + value.ToString();
+    }
+
+    public void ChangeScorePlayer2(int value)
+    {
+        ScorePlayer2.text = value.ToString() + "x";
+    }
+}
