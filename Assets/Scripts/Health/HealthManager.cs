c13af44f1ead5352653fe0c8ce450674fe319943
Kevin Aguilar <kevin.16.mn@gmail.com>
2020-02-01 15:57:04 -0400

    Add health bar

---
+++ Assets/Scripts/Health/HealthManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public static HealthManager instance;
+    public GameObject barn1;
+    //public GameObject barn2;
+
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            barn1.transform.localScale = new Vector3(4, barn1.transform.localScale.y, barn1.transform.localScale.z);
+        }
+    }
+
+    public void ChangeHealthBar1(int value)
+    {
+        barn1.transform.localScale = 
+            new Vector3((value*4.0f)/100.0f, barn1.transform.localScale.y, barn1.transform.localScale.z);  
+    }
+
+    //public void ChangeHealthBar2(int value)
+    //{
+    //    barn2.transform.localScale = 
+    //        new Vector3(value * 4 / 100, barn1.transform.localScale.y, barn1.transform.localScale.z);
+    //}
+}
---
+++ Assets/Scripts/Player/Health.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    private int health = 100;
+    private int max = 100;
+    public bool player;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Items"))
+        {
+            if(health > 0)
+            {
+                health -= 10;
+            }
+            if (player)
+            {
+                HealthManager.instance.ChangeHealthBar1(health);
+            }
+            //else
+            //{
+            //    HealthManager.instance.ChangeHealthBar2(health);
+            //}
+            Destroy(other.gameObject);
+        }
+    }
+}
